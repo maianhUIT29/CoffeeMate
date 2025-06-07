4aacb49adde6a0b62448c33dd772bed4b2d3f8cb
maianhUIT29 <23520065@gm.uit.edu.vn>
2025-06-08 00:08:31 +0700

    add_logout_btn

--- src/main/java/com/coffeemate/component/CashierHeader.java
+++ src/main/java/com/coffeemate/component/CashierHeader.java
@@ -4,6 +4,8 @@
  */
 package com.coffeemate.component;
 
+import com.coffeemate.utils.SessionManager;
+import com.coffeemate.view.LoginForm;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -12,6 +14,8 @@ import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.Window;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -36,6 +40,7 @@ public class CashierHeader extends javax.swing.JPanel {
     private void initComponents() {
 
         lblHeader = new javax.swing.JLabel();
+        jToggleButton1 = new javax.swing.JToggleButton();
 
         setForeground(new java.awt.Color(255, 255, 255));
 
@@ -43,12 +48,20 @@ public class CashierHeader extends javax.swing.JPanel {
         lblHeader.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
         lblHeader.setText("COFFEEMATE");
 
+        jToggleButton1.setText("ĐĂNG XUẤT");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(472, Short.MAX_VALUE)
+                .addComponent(jToggleButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 376, Short.MAX_VALUE)
                 .addComponent(lblHeader, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -57,9 +70,22 @@ public class CashierHeader extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(lblHeader)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(7, Short.MAX_VALUE))
+            .addComponent(jToggleButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+      // TODO add your handling code here:
+   // 1) Xóa session
+    SessionManager.getInstance().logout();
+    // 2) Mở LoginForm
+    new LoginForm().setVisible(true);
+    // 3) Đóng AdminView
+    Window window = SwingUtilities.getWindowAncestor(this);
+    if (window != null) {
+        window.dispose();
+    }    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
   @Override
     protected void paintComponent(Graphics g) {
@@ -76,6 +102,7 @@ public class CashierHeader extends javax.swing.JPanel {
 
   
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JLabel lblHeader;
     // End of variables declaration//GEN-END:variables
 }
